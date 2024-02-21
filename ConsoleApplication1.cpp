3bc9ff01579e09ffd9870800bbeb1a8340c9df02
ouxx2 <n1247@bk.ru>
2024-02-21 14:31:49 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(0, "rus");
+	int a, b, c;
+	cout << "Ввод двух чисел: " << endl;
+	cin >> a >> b;
+	c = a + b;
+	cout << "Сумма чисел: " << endl;
+}
